065c2e868c398194290a5d0c38fa1bc8441d0e48
ilorigideon2007 <ilorigideon2007@gmail.com>
2026-01-12 20:01:31 +0000

    updated

--- signup.js
+++
@@ -1,9 +0,0 @@
-function showForm(form) {
-  document.getElementById("signupForm").classList.add("hidden");
-  document.getElementById("loginForm").classList.add("hidden");
-  if (form === "signup") {
-    document.getElementById("signupForm").classList.remove("hidden");
-  } else {
-    document.getElementById("loginForm").classList.remove("hidden");
-  }
-}
